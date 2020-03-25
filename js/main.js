42cb400c93c209039bc0b63563a918c9357e7cff
Zerofelx <zeroflex3@gmail.com>
2020-03-25 03:02:40 -0500

    Agregando versión móvil, agregando métodos y depurando código.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
-var content = document.getElementById('content');
-var content1 = "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D122227899367758%26id%3D100660118191203&width=500"
-PrintContent(content1)
 
-
-setInterval(() => {
-    toggle()
-}, 15000);
-
-function toggle() {
-    content.classList.toggle('Toggle');
+var mobile = {
+    toggle: () => {
+        var content = document.getElementById('content-responsive');
+        content.classList.toggle('Toggle');
+    },
+    activate: () => {
+        setTimeout(() => {
+            mobile.toggle()
+        }, 3000);
+    }
 }
 
-function PrintContent(src) {
-    content.innerHTML = `
-    <iframe 
-    src="${src}" 
-    width="200" height="260" style="border:none;overflow:hidden" 
-    scrolling="no" frameborder="0" allowTransparency="true" 
-    allow="encrypted-media"></iframe> 
-    `
+var desktop = {
+    toggle: () => {
+        var content = document.getElementById('content');
+        content.classList.toggle('Toggle');
+    },
+    setContent: (src) => {
+        var content = document.getElementById('content');
+        content.innerHTML = `
+        <iframe 
+        src="${src}" 
+        width="200" height="260" style="border:none;overflow:hidden" 
+        scrolling="no" frameborder="0" allowTransparency="true" 
+        allow="encrypted-media"></iframe> 
+        `
+    },
+    activate() { 
+        var content1 = "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D122227899367758%26id%3D100660118191203&width=500"
+        desktop.setContent(content1)
+        setInterval(() => {
+            desktop.toggle()
+        }, 15000);
+    }
 }
+
+function init() {
+    var W = window.screen.width;
+    if(W > 720) {
+        desktop.activate();
+
+    } else if(W <= 720) {
+        mobile.activate();
+    }
+}
+
+init()
+
+
+
